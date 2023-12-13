79be51c8609bd4a790bc6d02f641d341d10d1e3d
Nidhi Sharma <nisharma@gainsight.com>
2023-12-13 16:24:50 +0530

    changed directory structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,5 @@
 import r2wc from '@r2wc/react-to-web-component'
-import { Counter, JOEngagement } from '../lib'
-
-window.customElements.get('px-counter') ||
-  window.customElements.define(
-    'px-counter',
-    r2wc(Counter, {
-      props: {
-        name: 'string',
-        onAction: 'function',
-        myObject: 'json'
-      },
-      shadow: 'open'
-    })
-  )
+import JOEngagement from '../lib'
 
 window.customElements.get('px-jo-engagement') ||
   window.customElements.define(
